5722dbcd1697cf3b58392a021327a478d580068a
thomashhs <345870016@qq.com>
2020-12-17 19:40:19 +0800

    20201217-2

--- fund/settings.py
+++ fund/settings.py
@@ -95,3 +95,4 @@ IMAGES_STORE = 'D:\\download_images'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+DOWNLOAD_DELAY = 1
--- fund/spiders/douban.py
+++ fund/spiders/douban.py
@@ -13,19 +13,37 @@ class MySpider1(scrapy.Spider):
         infos = json.loads(response.body.decode('utf-8'))
         cnt=0
         for info in infos['data']:
-            cnt+=1
-            if cnt==2:
-                break
             movie_url_detail = info['url']
-            yield scrapy.Request(movie_url_detail,callback=self.parse_movie)
+            yield scrapy.Request('https://movie.douban.com/subject/30257787/',callback=self.parse_movie)
 
     def parse_movie(self, response):
         line = response.xpath('//div[@id="info"]')
         item = DoubanMovieItem()
-        item['director'] = line.xpath('./span[1]/span[2]/a/text()').extract_first()
-        writer = line.xpath('./span[2]/span[2]/a')
+        #导演
+        director = line.xpath('./span[1]/span[2]')
+        item['director'] = director.xpath('string(.)').extract_first()
+        #编剧
+        writer = line.xpath('./span[2]/span[2]')
         item['writer'] = writer.xpath('string(.)').extract()
-        actor = line.xpath('.//span[@class="attrs"]/span')
+        #演员
+        actor = line.xpath('.//span[@class="actor"]/span[@class="attrs"]')
         item['actor'] = actor.xpath('string(.)').extract()
+        #分类
+        item['classify'] = '/'.join(line.xpath('.//span[@property="v:genre"]/text()').extract())
+        #上映日期
+        item['open_dt'] = line.xpath('.//span[@property="v:initialReleaseDate"]/text()').extract()
+        #电影时长
+        item['movie_length'] = line.xpath('.//span[@property="v:runtime"]/text()').extract()
+        #评级
+        item['rate'] = response.xpath('//strong[@property="v:average"]/text()').extract()
+        #评分人数
+        item['rate_num'] = response.xpath('//span[@property="v:votes"]/text()').extract()
+        #电影简介
+        movie_summary = response.xpath('.//span[@property="v:summary"]')
+        item['movie_summary'] = movie_summary.xpath('string(.)').extract_first()
+        #电影名称
+        item['movie_name'] = response.xpath('//span[@property="v:itemreviewed"]/text()').extract()
+        #年份
+        item['movie_year'] = response.xpath('//span[@class="year"]/text()').extract()
 
         yield item
